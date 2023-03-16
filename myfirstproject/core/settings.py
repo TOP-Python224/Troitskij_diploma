d70e350b40a6cad121a6099cd4f2f4c6e3fadc4f
TigranTroitskiy <daredevil30000@gmail.com>
2023-03-16 21:23:30 +0300

    решение проблемы с путем до БД

--- myfirstproject/core/settings.py
+++ myfirstproject/core/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from platform import node
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+pymysql.install_as_MySQLdb()
 
 
 # Quick-start development settings - unsuitable for production
